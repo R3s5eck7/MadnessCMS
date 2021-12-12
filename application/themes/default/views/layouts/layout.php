f50aac8292dbf84684b44c1c6a2ea1b8a02cf250
R3s5eck7 <95909668+R3s5eck7@users.noreply.github.com>
2021-12-13 00:50:17 +0200

    Update layout.php

--- application/themes/default/views/layouts/layout.php
+++ application/themes/default/views/layouts/layout.php
@@ -222,7 +222,7 @@
           <a target="_blank" href="<?= $this->config->item('social_youtube'); ?>" class="uk-icon-button"><i class="fab fa-youtube"></i></a>
         </div>
         <p class="uk-text-center uk-margin-small">Copyright <i class="far fa-copyright"></i> <?= date('Y'); ?> <span class="uk-text-bold"><?= $this->config->item('website_name'); ?></span>. <?= $this->lang->line('footer_rights'); ?></p>
-        <p class="uk-text-small uk-margin-small uk-text-center">World of Warcraft® and Blizzard Entertainment® are all trademarks or registered trademarks of Blizzard Entertainment in the United States and/or other countries. These terms and all related materials, logos, and images are copyright © Blizzard Entertainment. This site is in no way associated with or endorsed by Blizzard Entertainment®.</p>
+        <p class="uk-text-small uk-margin-small uk-text-center">World of Warcraft® and Blizzard Entertainment® are all trademarks or registered trademarks of Blizzard Entertainment in the United States and/or other countries.. These terms and all related materials, logos, and images are copyright © Blizzard Entertainment. This site is in no way associated with or endorsed by Blizzard Entertainment®.</p>
         <p class="uk-h6 uk-text-bold uk-text-uppercase uk-margin-small uk-text-center">Powered by <a target="_blank" href="https://global-wow.com">Madness® The free license of BlizzCMS</a></p>
       </div>
     </section>
